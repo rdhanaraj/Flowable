a1899b7f2fea326def8e5aea83656cdde7ed64ac
Rishi Dhanaraj <rishid97@gmail.com>
2014-07-08 16:01:33 -0700

    Add Company model, associations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ActiveRecord::Base
+  has_many :users, -> { where admin: false }
+  belongs_to :user, -> { where admin: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :companies if admin
+  belongs_to :company unless admin
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
